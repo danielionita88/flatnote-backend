ddb5c001db637c53628871fe998ad55c210143a5
Daniel Ionita <danielionita88@yahoo.com>
2020-01-29 15:56:30 -0600

    create tags and delete them when update

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -12,7 +12,15 @@ class NotesController < ApplicationController
 
     def create
         note = Note.create(title: params[:title], content: params[:content], user_id: params[:user_id])
-        render json: note
+        if params[:tags].length > 0
+            tags = params['tags'].map{|tag| 
+                if tag.length>= 3
+                Tag.find_or_create_by(name: tag)
+                end
+            }
+            note_tags= tags.map{|tag| NoteTag.find_or_create_by(tag_id: tag.id, note_id: note.id)}
+        end
+        render json: note, include: :tags
     end
 
     def show
@@ -25,16 +33,21 @@ class NotesController < ApplicationController
 
     def update
         note=Note.find(params[:id])
-  
-        tags = params[:tags].map{|tag|
+        params[:tags].map{|tag|
             if tag.length >= 3 
             tag = Tag.find_or_create_by(name:tag)
             NoteTag.find_or_create_by(tag_id: tag.id, note_id: note.id)
             end
         }
-        
+        tags_name= note.tags.map{|tag| tag.name }
+        deleted_tags = tags_name - params[:tags]
+        deleted_tags.map{|name| 
+        tag=Tag.find_by(name: name)
+        NoteTag.find_by(tag_id: tag.id, note_id: note.id).destroy
+        }
         note.update(title: params[:title],
         content: params[:content])
+        note=Note.find(params[:id])
         
         render json: note, include: :tags
     end
